502d6ec8f91229831eed195e5be410bc89c21d68
sudhip kumar <123609652+sudhipkumar@users.noreply.github.com>
2025-03-08 22:04:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
-function mincost(arr)
-{ 
-//write your code here
-// return the min cost
-  
+function mincost(arr) {
+    if (arr.length === 1) return 0; // No cost if only one rope
+
+    const minHeap = new MinPriorityQueue(); // Use a min heap
+
+    // Insert all elements into the min heap
+    for (let num of arr) {
+        minHeap.enqueue(num);
+    }
+
+    let totalCost = 0;
+
+    // Process the heap until we have one rope left
+    while (minHeap.size() > 1) {
+        let first = minHeap.dequeue().element;
+        let second = minHeap.dequeue().element;
+        let cost = first + second;
+        totalCost += cost;
+        minHeap.enqueue(cost);
+    }
+
+    return totalCost;
 }
 
-module.exports=mincost;
+module.exports = mincost;
